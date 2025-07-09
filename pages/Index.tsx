d2fb24b853da51846e16b0204001335824c22e62
d00kSI <81750536+d00kSI@users.noreply.github.com>
2025-07-10 00:00:43 +0200

    Added the basic structure
    Added the basic empty structure, including:
    - main app (App.tsx),
    - pages (Index.tsx and NotFound.tsx),and
    - sections (Navigation, Intro, About, DataAnalysis, GraphicDesign, Coding, CV, Contact, and Footer)

---
+++ pages/Index.tsx
@@ -0,0 +1,27 @@
+import Navigation from "@/components/ui/navigation";
+import Intro from "@/components/sections/Intro";
+import About from "@/components/sections/About";
+import DataAnalysis from "@/components/sections/DataAnalysis";
+import GraphicDesign from "@/components/sections/GraphicDesign";
+import Coding from "@/components/sections/Coding";
+import CV from "@/components/sections/CV";
+import Contact from "@/components/sections/Contact";
+import Footer from "@/components/sections/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <Intro />
+      <About />
+      <DataAnalysis />
+      <GraphicDesign />
+      <Coding />
+      <CV />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx
+import './index.css';
+
+// Importing the main application component and rendering it into the root element
+createRoot(document.getElementById('root')).render(<App />);
