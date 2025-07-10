c02b5fa1442f0f637301e7c70e8fa60c6823abb9
d00kSI <81750536+d00kSI@users.noreply.github.com>
2025-07-10 08:46:59 +0200

    Implemented ThemeProvider
    Implemented ThemeProvider to set up and configure the themes, a wrapper around the next-themes library.

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,36 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ToastProvider } from "@/hooks/use-toast";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
+const queryClient = new QueryClient();
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem={false}
+      disableTransitionOnChange={false}
+      themes={["light", "low-light", "dark"]}
+    >
+        <TooltipProvider>
+        <ToastProvider> 
+            <Toaster />
+            <Sonner />
+            <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+            </BrowserRouter>
+        </ToastProvider>
+        </TooltipProvider>
+    </ThemeProvider>
   </QueryClientProvider>
 );
 
