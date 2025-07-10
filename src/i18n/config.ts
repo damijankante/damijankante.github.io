04add1a0cd444bb94e77761ab5b7135078cb5728
d00kSI <81750536+d00kSI@users.noreply.github.com>
2025-07-10 08:19:57 +0200

    Added translation files
    Added and defined translation files for English, German and Slovenian versions

---
+++ src/i18n/config.ts
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './locales/en.json';
+import de from './locales/de.json';
+import sl from './locales/sl.json';
+
+const resources = {
+  en: { translation: en },
+  de: { translation: de },
+  sl: { translation: sl },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
