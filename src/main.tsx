ff32cd031c799b5b4d44b2a225e958c3a66da7c7
d00kSI <81750536+d00kSI@users.noreply.github.com>
2025-07-10 08:42:36 +0200

    Added i18n config file
    Added i18n config file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx'
-import './index.css';
+import './index.css'
+import './i18n/config'
 
 // Importing the main application component and rendering it into the root element
 createRoot(document.getElementById("root")!).render(<App />);
