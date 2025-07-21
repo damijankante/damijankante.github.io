e2be796ee0e19ef8c08697244268fa8b87b1493f
d00kSI <81750536+d00kSI@users.noreply.github.com>
2025-07-22 00:42:50 +0200

    Updated main.tsx
    Wrapped the entire `<App />` component with the <HelmetProvider> to provide the necessary context for `react-helmet-async` to function throughout the application.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx'
 import './index.css'
 import './i18n/config'
+import { HelmetProvider } from 'react-helmet-async';
 
 // Importing the main application component and rendering it into the root element
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<HelmetProvider><App /></HelmetProvider>);
